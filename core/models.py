edeff0a163fd3229cc70a0d52b10a0935dcf5871
dayvison1 <dayvison-diogenes@hotmail.com>
2020-09-22 17:13:00 -0300

    migrates

--- core/models.py
+++ core/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Empresa(models.Model):
+    nomeEmpresa = models.CharField(max_length=255)
+    razaoSocial = models.CharField(max_length=255)
+    responsavel = models.CharField(max_length=255)
+    endereço = models.CharField(max_length=255)
+    cpf = models.CharField(max_length=255)
+    telefone = models.CharField(max_length=255)
+    celular = models.CharField(max_length=255)
+    email = models.EmailField()
+    dataCadastroEmp = models.DateTimeField(auto_now_add=True)
+    dataUpdateEm = models.DateTimeField(auto_now=True)
+    
